b8f079ee218d833353f34062846b37da93ae5a96
Bridget Reeves <Bridgetreeves@Bridgets-MacBook-Air.local>
2016-04-26 21:28:33 -0700

    added for first project

--- First/ViewController.swift
+++ First/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blueimg: UIImageView!
+    
+    @IBOutlet weak var redimg: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func hideblue(sender: AnyObject) {
+        blueimg.hidden = true
+    }
+    @IBAction func hidered(sender: AnyObject) {
+        redimg.hidden = true
+    }
 
 }
 
